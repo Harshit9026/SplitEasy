32ced9fba1445c2dc0aad5e394b47562369f3648
Harshit9026 <harshitshukla813@gmail.com>
2026-04-11 14:48:48 +0530

    build error resolved

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
-import MembershipClaimer from '.@/components/MembershipClaimer'
+import MembershipClaimer from '@/components/MembershipClaimer'
 import './globals.css'
 
 const _geist = Geist({ subsets: ["latin"] });
